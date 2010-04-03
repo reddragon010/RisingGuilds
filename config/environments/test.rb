a559a0e195d092aa2f023043c3dc49042a16572e
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-04-03 05:58:45 +0200

    added factory_girl-gem

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,3 +32,4 @@ config.gem "rspec-rails", :lib => false, :version => ">=1.2.2"
 config.gem "webrat", :lib => false, :version => ">=0.4.3"
 config.gem "cucumber", :lib => false, :version => ">=0.2.2"
 config.gem "cucumber-rails", :lib => false
+config.gem "factory_girl"
