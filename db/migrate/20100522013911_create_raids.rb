1daf07de40d67d0e304d8d241060694660ce9d91
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-05-22 03:49:01 +0200

    changed some :null and :default-values for raid/attendance

--- db/migrate/20100522013911_create_raids.rb
+++ db/migrate/20100522013911_create_raids.rb
@@ -1,15 +1,15 @@
 class CreateRaids < ActiveRecord::Migration
   def self.up
     create_table :raids do |t|
-      t.integer :guild_id
-      t.string :title
-      t.integer :max_attendees
+      t.integer :guild_id, :null => false
+      t.string :title, :null => false
+      t.integer :max_attendees, :default => 25
       t.datetime :invite_start
-      t.datetime :start
+      t.datetime :start, :null => false
       t.datetime :end
       t.text :description
       t.integer :leader
-      t.boolean :closed
+      t.boolean :closed, :null => false, :default => false
 
       t.timestamps
     end
--- db/migrate/20100522014428_create_attendances.rb
+++ db/migrate/20100522014428_create_attendances.rb
@@ -1,10 +1,10 @@
 class CreateAttendances < ActiveRecord::Migration
   def self.up
     create_table :attendances do |t|
-      t.integer :character_id
-      t.integer :raid_id
-      t.string :role
-      t.integer :status
+      t.integer :character_id, :null => false
+      t.integer :raid_id, :null => false
+      t.string :role, :null => false
+      t.integer :status, :null => false, :default => 1
       t.sting :message
 
       t.timestamps
