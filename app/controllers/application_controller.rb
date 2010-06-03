efcd25a8a7ff16e0a93b296dca32462b08200ae2
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-06-03 15:37:09 +0200

    changed main- to app-layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  layout "main"
   include Authentication
 
   helper :all # include all helpers, all the time
