a743526a2bff20203f3808838602df78ffff775f
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-06-04 13:32:07 +0200

    some tiny changes on drantum's commits

--- db/migrate/20100601133128_change_raid_table.rb
+++ db/migrate/20100601133128_change_raid_table.rb
@@ -1,7 +1,7 @@
 class ChangeRaidTable < ActiveRecord::Migration
   def self.up
 	change_table :raids do |t|
-		t.column :date, :date, :null => false
+		t.column :date, :date, :null => false, :default => 'now()'
     t.change :invite_start, :time
     t.change :start, :time
     t.change :end, :time
@@ -12,12 +12,10 @@ class ChangeRaidTable < ActiveRecord::Migration
 
   def self.down
 	change_table :raids do |t|
-		t.column :date, :date, :null => false
-    t.change :invite_start, :time
-    t.change :start, :time
-    t.change :end, :timee
-    t.column :max_lvl, :int, :null => true
-    t.column :min_lvl, :int, :null => true
+    t.change :invite_start, :datetime
+    t.change :start, :datetime
+    t.change :end, :datetime
+    t.remove :max_lvl, :min_lvl, :date
     end
   end
 end
