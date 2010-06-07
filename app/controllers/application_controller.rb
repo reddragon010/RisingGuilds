244e468cfe51fea1014309c6bec4618a168ebe83
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-06-07 20:16:39 +0200

    better tabs handling and guilds-user-index added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,39 @@
 
 class ApplicationController < ActionController::Base
   include Authentication
-
+  
+  layout :choose_layout
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   before_filter { |c| Authorization.current_user = c.current_user}
   before_filter :write_return_to
-  
+  before_filter :setup_tabs
   
   protected
+  
+  def setup_tabs
+    @tabs = Array.new
+  end
+  
+  def choose_layout
+    case controller_name
+    when "guilds"
+      return "guild_tabs" if params[:guild_id] || action_name != 'index'
+    when "characters"
+      if params[:guild_id]
+        return "guild_tabs" 
+      else
+        return "character_tabs"
+      end
+    when "raids"
+      return "guild_tabs" if params[:guild_id]
+    when "users"
+      return "guild_tabs" if params[:guild_id]
+    end
+    return "application"
+  end
   
   def permission_denied
     flash[:error] = "Sorry, you are not allowed to access that page."
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :login_required, :except => [:new, :create]
+  layout :choose_layout, :only => [:index]
   
   def index
     @users = User.all
