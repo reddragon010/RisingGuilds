e7cf6ce88a161cd94f1b5660b65c6e4916756f63
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-06-14 14:34:22 +0200

    new-guilds-tab forgotten ... oops / hide usertabs if not logged in

--- db/migrate/20100601133128_change_raid_table.rb
+++ db/migrate/20100601133128_change_raid_table.rb
@@ -1,7 +1,7 @@
 class ChangeRaidTable < ActiveRecord::Migration
   def self.up
 	change_table :raids do |t|
-		t.column :date, :date, :null => false, :default => 'now()'
+		t.column :date, :date, :null => false
     t.change :invite_start, :time
     t.change :start, :time
     t.change :end, :time
