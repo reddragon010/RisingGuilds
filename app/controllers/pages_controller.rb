890cdd5d9f7caf075870eadfd824b25cc9b5a6a9
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-07-25 00:50:25 +0200

    changed default static-page language to german

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,7 @@ class PagesController < ApplicationController
   end
 
   def current_page
-    "pages/#{I18n.locale}/#{File.join(*params[:id]).downcase}"
+    #"pages/#{I18n.locale}/#{File.join(*params[:id]).downcase}"
+    "pages/de/#{File.join(*params[:id]).downcase}"
   end  
 end
