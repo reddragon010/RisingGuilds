a826acff0c9d9e597e216f24cb0b9927d89eb80c
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-09-24 02:45:19 +0200

    updated routes to rails3

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,52 @@
-RisingGuilds::Application.routes.draw do |map|
-   map.connect 'pages/*id', :controller => 'pages', :action => 'show'
+RisingGuilds::Application.routes.draw do 
+   root :to =>"pages#show", :id => "home"
+   
+   # Static Pages Route
+   match 'pages/*id' => 'pages#show'
+   
+   # Authentication Routes
+   match "/account" => "users#show", :as => :account
+   match '/logout' => 'user_sessions#destroy', :as => :logout
+   match '/login' => 'user_sessions#new', :as => :login
+   match '/register' => 'users#create', :as => :register
+   match '/signup' => 'users#new', :as => :signup
+   match '/rating' => 'rating#index', :as => :rating #:register
+   match '/register/:activation_code' => 'activations#new', :as => :register
+   match '/activate/:id' => 'activations#create', :as => :activate
+   
+   #Resources
+   resources :password_resets
+   resource :user_session
+   resources :users do
+     resources :characters
+     resources :guilds
+   end
+       
+   resources :guilds do
+     resources :characters
+     resources :users
+     resources :raids
+     resources :newsentries
+     resources :events
+   end
+   resources :characters do
+     resources :events
+   end
+   resources :events
+   resources :newsentries
+   
+   resources :raids do
+     resources :attendances
+   end
+   resources :attendances
 
-   map.resources :newsentries
-   map.resources :events
-   map.resource :user_session
-   map.resources :password_resets
-   map.resource :account, :controller => "users"
-
-   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
-   map.login '/login', :controller => 'user_sessions', :action => 'new'
-   map.register '/register', :controller => 'users', :action => 'create'
-   map.signup '/signup', :controller => 'users', :action => 'new'
-
-   map.rating '/rating', :controller => 'rating', :action => 'index'
-
-   map.register '/register/:activation_code', :controller => 'activations', :action => 'new'
-   map.activate '/activate/:id', :controller => 'activations', :action => 'create'
-
-   map.resources :guilds, :has_many => [:characters, :users, :raids, :newsentries]
-   map.resources :characters
-   map.resources :users, :has_many => [:characters, :guilds]
-   map.resources :raids, :has_many => [:attendances]
-   map.resources :attendances
-
-   map.root :controller => "pages", :action => :show, :id => 'home'
-
-   map.connect '/guilds/:id/join/:token', :controller => 'guilds', :action => 'join', :token => /.*/
-   map.connect '/widget/:action/:id/:token', :controller => 'widget', :token => /.*/
-
-   map.connect ':controller/:id/:action'
-   map.connect ':controller/:id/:action.:format'
+   
+   #Guild Join Route
+   match '/guilds/:id/join/:token', :controller => 'guilds', :action => 'join'
+   
+   #Widget Auth Route
+   match '/widget/:action/:id/:token', :controller => 'widget'
+   
+   #Default Route
+   match ':controller/:id/:action(.:format)'
 end
