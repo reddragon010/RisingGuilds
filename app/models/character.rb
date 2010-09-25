d33f51c0f223b3de2ee951680db68578e5373a0b
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-09-25 04:16:13 +0200

    extracted arsenal-classes into own file

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,10 +5,10 @@ class Character < ActiveRecord::Base
   belongs_to :user
   has_many :attendances
   
-  serialize :profession1
-  serialize :profession2
-  serialize :talentspec1
-  serialize :talentspec2
+  serialize :profession1, Arsenal::Profession
+  serialize :profession2, Arsenal::Profession
+  serialize :talentspec1, Arsenal::TalentSpec
+  serialize :talentspec2, Arsenal::TalentSpec
   serialize :items
   
   validates_uniqueness_of :name
@@ -16,47 +16,5 @@ class Character < ActiveRecord::Base
   
   def netto_activity
     Integer((self.activity / ((Time.now - self.created_at) / 60 / 60)) * 100) unless self.activity.nil?
-  end
-  
-  class TalentSpec
-  	attr_reader :trees,:active,:group,:icon,:prim
-
-  	def initialize(elem)
-  		@trees = []
-  		@trees[1] = elem[:treeOne].to_i
-  		@trees[2] = elem[:treeTwo].to_i
-  		@trees[3] = elem[:treeThree].to_i
-
-      @active = elem[:active].nil? ? false : true
-      @group  = elem[:group].to_i
-      @icon   = elem[:icon]
-      @prim   = elem[:prim]
-  	end
-  end
-
-  class Profession
-  	attr_reader :key, :name, :value, :max
-  	alias_method :to_s, :name
-  	alias_method :to_i, :value
-
-  	def initialize(elem)
-  		@key 		= elem[:key]
-  		@name 	= elem[:name]
-  		@value 	= elem[:value].to_i
-  		@max 		= elem[:max].to_i
-  	end
-  end
-
-  class Item
-  	attr_reader :id, :icon, :level
-
-  	def initialize(elem)
-  		@id 				= elem[:id].to_i
-  		@icon 	    = elem[:icon]
-  	end
-  	
-  	def get_info(elem)
-  	  @level = (elem%'itemInfo'%'item')[:level].to_i
-  	end
   end
 end
