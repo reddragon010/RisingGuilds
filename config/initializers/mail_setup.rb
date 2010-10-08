603cb0d85d391e919bbf002d2fe1de8fd0da2030
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-10-08 22:08:52 +0200

    trying to fix the OpenSSL::SSL::SSLError

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,2 +1,3 @@
 ActionMailer::Base.default_url_options[:host] = configatron.notifier.default_url
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
