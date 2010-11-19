fff5d17506f44fda5bc2eed1b8698eed3d8582dd
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-11-19 14:36:06 +0100

    finished statistics refs #61

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
+  layout :choose_layout
   
   def index
     @guild = Guild.find(params[:guild_id])
@@ -7,8 +8,26 @@ class StatisticsController < ApplicationController
       @online_start = online_events.first.created_at
       @online_data = online_events.all.map{|e| e.content.to_i}
     end
-    @growth_data = @guild.events.where(:action => 'weekly_members')
-    @ail_data = @guild.events.where(:action => 'weekly_ail')
+    growth_events = @guild.events.where(:action => 'weekly_members').order('created_at')
+    unless growth_events.all.blank?
+      @growth_start = growth_events.first.created_at
+      @growth_data = growth_events.all.map{|e| e.content.to_i}
+    end
+    ail_events = @guild.events.where(:action => 'weekly_ail')
+    unless ail_events.all.blank?
+      @ail_start = ail_events.first.created_at
+      @ail_data = ail_events.all.map{|e| e.content.to_i}
+    end
+     
   end
   
+  protected
+  
+  def choose_layout
+    unless params[:guild_id].nil?
+      return 'guild_tabs'
+    else
+      return 'application'
+    end
+  end
 end
