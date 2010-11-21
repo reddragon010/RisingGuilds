8ea73ba63667da52ec567029e5d25b4ed421bdfd
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-11-21 21:39:43 +0100

    added tabs to statistics refs #61

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -11,17 +11,17 @@ class StatisticsController < ApplicationController
     online_events = @guild.events.where(:action => 'today_online').order('created_at')
     unless online_events.all.blank?
       @online_start = online_events.first.created_at
-      @online_data = online_events.all.map{|e| e.content.to_i}
+      @online_data = online_events.all.map{|e| [e.created_at.to_i * 1000, e.content.to_i]}
     end
     growth_events = @guild.events.where(:action => 'weekly_members').order('created_at')
     unless growth_events.all.blank?
       @growth_start = growth_events.first.created_at
-      @growth_data = growth_events.all.map{|e| e.content.to_i}
+      @growth_data = growth_events.all.map{|e| [e.created_at.to_i  * 1000, e.content.to_i]}
     end
     ail_events = @guild.events.where(:action => 'weekly_ail')
     unless ail_events.all.blank?
       @ail_start = ail_events.first.created_at
-      @ail_data = ail_events.all.map{|e| e.content.to_i}
+      @ail_data = ail_events.all.map{|e| [e.created_at.to_i  * 1000, e.content.to_i]}
     end
   end
   
