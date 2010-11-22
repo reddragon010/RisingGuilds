f981143c99eebc8a14ef2d0c795c74548ea3e891
Michael Riedmann <mriedmann@RRIs-MacMini.local>
2010-11-22 01:14:29 +0100

    fixing role-convert-migration

--- db/migrate/20101117160035_migrate_roles_to_new_format.rb
+++ db/migrate/20101117160035_migrate_roles_to_new_format.rb
@@ -1,13 +1,17 @@
 class MigrateRolesToNewFormat < ActiveRecord::Migration
-#  def self.up
-#    Assignment.all.each do |a|
-#      a.update_attribute :role, Role.find(a.role_id).name
-#    end
-#  end
+  def self.up
+    if Assignment.column_names.include?('role_id') && Assignment.column_names.include?('role')
+      Assignment.all.each do |a|
+        a.update_attribute :role, Role.find(a.role_id).name
+      end
+    end
+  end
 
-# def self.down
-#    Assignment.all.each do |a|
-#      a.update_attribute :role_id, Role.find_by_name(a.role).id
-#    end
-#  end
+ def self.down
+    if Assignment.column_names.include?('role_id') && Assignment.column_names.include?('role')
+      Assignment.all.each do |a|
+        a.update_attribute :role_id, Role.find_by_name(a.role).id
+      end
+    end
+  end
 end
